42aa6c201160d2e47129f6aaf522c315393fb2c7
angelgamo <garcia.angel.69420@gmail.com>
2025-05-25 11:18:48 +0200

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import '../app.css';
+	import '$lib/fonts/fonts.css';
+
+	let { children } = $props();
+</script>
+
+<div class="flex justify-between absolute w-full max-w-[1600px] p-5 text-Secundario *:*:hover:text-[var(--Principal)] text-white">
+	<div class="flex gap-x-2 *:pr-2 divide-x-1 divide-solid divide-white">
+		<a href="_blank">Fuster & López</a>
+		<a href="_blank">Empresas</a>
+		<a href="_blank">Conozenos</a>
+		<a href="_blank">Fundación</a>
+		<a href="_blank">Proyectos</a>
+		<a href="_blank">Contacto</a>
+	</div>
+	<div class="flex gap-x-2 *:pr-3 divide-x-1 divide-solid">
+		<a href= "mailto: fusterandlopez@fusterandlopez.com">fusterandlopez@fusterandlopez.com</a>
+		<a href="tel:34943202293">+34 943 20 22 93</a>
+	</div>
+</div>
+
+{@render children()}
+
+<div class="py-20 px-8 flex flex-col gap-3 relative text-white">
+    <div class="absolute inset-0 left-[-33vw] right-[-33vw] z-[-1] bg-[rgb(16,7,9)] bg-[url($lib/assets/images/pattern.png)]"></div>
+	<div class="m-auto flex flex-col items-center *:text-[var(--Principal)]">
+		<enhanced:img src="$lib/assets/images/Logo_FB.png?grayscale&w=150" alt="An alt text"/>
+		<a class="text-Enfasis" href= "mailto: fusterandlopez@fusterandlopez.com">fusterandlopez@fusterandlopez.com</a>
+		<a class="text-Enfasis" href="tel:34943202293">+34 943 20 22 93</a>
+	</div>
+</div>
+<div class="bg-[var(--Principal)] p-2 w-screen absolute left-0">
+	<p class="text-Texto text-center text-white">Todos los derechos reservados ©Fuster&Lopez 2025 </p>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,92 @@
+<div class="h-dvh flex flex-col justify-center items-center">
+    <div class="background-image">
+        <enhanced:img src="$lib/assets/images/OIP.jpg?grayscale&blur" alt="An alt text" sizes="min-height(322px, 100dvh)" class="background-image" />
+    </div>
+    <enhanced:img src="$lib/assets/images/Logo_FD.png?w=250" alt="An alt text"/> 
+    <h1 class="text-Principal text-[var(--Principal)] text-center">Solidez, Fiabilidad
+        <br>
+        <strong>Futuro exitoso</strong>
+    </h1>
+
+</div>
+
+<div class="py-15 px-8 flex flex-col gap-3">
+    <h1 class="text-Principal text-[var(--Principal)]">Fuster & López</h1>
+    <p class="text-Enfasis">Empresa patrimonial que desempeña un papel crucial al controlar y <strong>proporcionar dirección estratégica</strong> a sus unidades de negocio, al mismo tiempo busca inversores para financiar proyectos tanto en el sector de importación/exportación, inmobiliario y de restauración.</p>
+    <br>
+    <div class="flex justify-center gap-15">
+        <div class="flex flex-col gap-5 w-1/2 py-5 items-stretch">
+            <p class="text-Secundario"><strong>Grandes Inversores</strong></p>
+            <p class="text-Texto">Fuster & Lopez identificará y se asociará con grandes <strong>inversores, como fondos de capital privado</strong>, firmas de inversión y grupos empresariales, para asegurar inversiones significativas en los proyectos que llevemos a cabo. Estas inversiones proporcionarán <strong>capital sólido y respaldo estratégico para impulsar el crecimiento y la expansión de las unidades de negocio.</strong></p>
+            <br>
+            <p class="text-Secundario"><strong>Crowfounding</strong></p>
+            <p class="text-Texto">En Fuster&Lopez, ejercemos un control activo y efectivo sobre nuestras unidades de negocio. Esto se traduce en una supervisión rigurosa de las operaciones, la optimización de recursos y la identificación de oportunidades de crecimiento. Nuestro equipo directivo cuenta con una vasta experiencia en diversos sectores. Jo que nos permite tomar decisiones informadas y oportunas para maximizar el valor de nuestras inversiones.</p>
+        </div>
+        <div class="w-1/2 m-auto">
+                    <enhanced:img src="$lib/assets/images/OIP.jpg?grayscale" alt="An alt text" class="w-full h-aspect-[474/322]"/>
+                    <!-- <div class="w-full h-full bg-red-500"></div> -->
+        </div>
+    </div>
+</div>
+
+<div class="py-15 px-8 flex flex-col gap-3 relative text-white">
+    <div class="absolute inset-0 left-[-33vw] right-[30%] z-[-1] bg-[rgb(16,7,9)] bg-[url($lib/assets/images/pattern.png)]"></div>
+    <h1 class="text-Principal text-[var(--Principal)]">Misión y <strong>Valores</strong></h1>
+    <div class="w-1/2 flex flex-col gap-8">
+        <p class="text-Enfasis">Nuestra misión es <strong>crear valor sostenible para nuestros accionistas</strong>, clientes y socios a través de una gestión sólida y una visión estratégica a largo plazo. <strong>Nos esforzamos por ser líderes en los sectores en los que participamos, manteniendo altos estándares de ética empresarial y responsabilidad social.</strong></p>
+        <p class="text-Texto">En Fuster&Lopez, ejercemos un <strong>control activo y efectivo sobre nuestras unidades de negocio</strong>. Esto se traduce en una supervisión rigurosa de las operaciones, la optimización de recursos y la identificación de oportunidades de crecimiento. Nuestro equipo directivo cuenta con una vasta experiencia en diversos sectores. Jo que nos permite tomar decisiones informadas y oportunas para maximizar el valor de nuestras inversiones.</p>
+    </div>
+</div>
+
+<div class="py-15 px-8 flex flex-col gap-3">
+    <h1 class="text-Principal text-[var(--Principal)]"><strong>Empresas</strong></h1><br>
+    <p class="text-Enfasis max-w-[1100px]">En Fuster&Lopez, nos enorgullecemos de nuestra <strong>capacidad para controlar y proporcionar dirección estratégica a nuestras diversas unidades de negocio</strong>. Nuestro papel crucial en la gestión de activos y en la toma de decisiones estratégicas nos ha permitido mantener una posición sólida y competitiva en los mercados en los que operamos.</p>
+    <br>
+    <div class="flex text-Texto *:p-10 *:w-full *:*:mb-5">
+        <div class=" bg-[var(--Principal)] text-white">
+            <div class="h-[55px]"><svg aria-hidden="true" class="max-w-[50px] max-h-[50px]" viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg"><path d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z"></path></svg></div>
+            <div><enhanced:img src="$lib/assets/images/lideria.png" alt="An alt text" height=60/></div>
+            <p><strong>Invierte con confianza en nuestro portafolio inmobiliario</strong> sólido y seguro. Nuestra experiencia y solidez en el sector inmobiliario garantizan rentabilidad a largo plazo.Construyendo un futuro estable y fiable a través de nuestras <strong>inversiones inmobiliarias.</strong></p>
+        </div>
+        <div class="bg-[#F2F2F2]">
+            <div class="h-[55px]"><svg aria-hidden="true" class="max-w-[50px] max-h-[50px] fill-[var(--Principal)]" viewBox="0 0 496 512" xmlns="http://www.w3.org/2000/svg"><path d="M336.5 160C322 70.7 287.8 8 248 8s-74 62.7-88.5 152h177zM152 256c0 22.2 1.2 43.5 3.3 64h185.3c2.1-20.5 3.3-41.8 3.3-64s-1.2-43.5-3.3-64H155.3c-2.1 20.5-3.3 41.8-3.3 64zm324.7-96c-28.6-67.9-86.5-120.4-158-141.6 24.4 33.8 41.2 84.7 50 141.6h108zM177.2 18.4C105.8 39.6 47.8 92.1 19.3 160h108c8.7-56.9 25.5-107.8 49.9-141.6zM487.4 192H372.7c2.1 21 3.3 42.5 3.3 64s-1.2 43-3.3 64h114.6c5.5-20.5 8.6-41.8 8.6-64s-3.1-43.5-8.5-64zM120 256c0-21.5 1.2-43 3.3-64H8.6C3.2 212.5 0 233.8 0 256s3.2 43.5 8.6 64h114.6c-2-21-3.2-42.5-3.2-64zm39.5 96c14.5 89.3 48.7 152 88.5 152s74-62.7 88.5-152h-177zm159.3 141.6c71.4-21.2 129.4-73.7 158-141.6h-108c-8.8 56.9-25.6 107.8-50 141.6zM19.3 352c28.6 67.9 86.5 120.4 158 141.6-24.4-33.8-41.2-84.7-50-141.6h-108z"></path></svg></div>
+            <div class="h-[60px] content-center"><enhanced:img src="$lib/assets/images/xportinport.png" alt="An alt text" height=30/></div>
+            <p>Conectando <strong>mercados globales con solidez y seguridad</strong> en cada transacción de importación/exportación. Nuestra red internacional garantiza <strong>fiabilidad y confianza en el comercio exterior</strong>. Invierte en un futuro próspero y seguro con nuestras operaciones de importación/exportación.</p>
+        </div>
+        <div class="bg-[var(--Enfasis)]">
+            <div class="h-[55px]"><svg aria-hidden="true" class="max-w-[50px] max-h-[50px] fill-[var(--Principal)]" viewBox="0 0 416 512" xmlns="http://www.w3.org/2000/svg"><path d="M207.9 15.2c.8 4.7 16.1 94.5 16.1 128.8 0 52.3-27.8 89.6-68.9 104.6L168 486.7c.7 13.7-10.2 25.3-24 25.3H80c-13.7 0-24.7-11.5-24-25.3l12.9-238.1C27.7 233.6 0 196.2 0 144 0 109.6 15.3 19.9 16.1 15.2 19.3-5.1 61.4-5.4 64 16.3v141.2c1.3 3.4 15.1 3.2 16 0 1.4-25.3 7.9-139.2 8-141.8 3.3-20.8 44.7-20.8 47.9 0 .2 2.7 6.6 116.5 8 141.8.9 3.2 14.8 3.4 16 0V16.3c2.6-21.6 44.8-21.4 48-1.1zm119.2 285.7l-15 185.1c-1.2 14 9.9 26 23.9 26h56c13.3 0 24-10.7 24-24V24c0-13.2-10.7-24-24-24-82.5 0-221.4 178.5-64.9 300.9z"></path></svg></div>
+            <div><enhanced:img src="$lib/assets/images/casa-fuster.png" alt="An alt text" height=60/></div>
+            <p>Descubre la <strong>excelencia gastronómica respaldada por nuestra reputación de solidez y seguridad</strong>. Saborea la confianza en cada plato que ofrecemos, gracias a nuestra trayectoria fiable en restauración.<strong>Inversiones en restauración</strong> que ofrecen estabilidad y fiabilidad para nuestros socios.</p>
+        </div>
+    </div>
+</div>
+
+<div class="py-10 px-8 flex flex-col gap-3">
+    <h1 class="text-Principal text-[var(--Principal)]"><strong>Oportunidades de inversión</strong></h1><br>
+    <p class="text-Enfasis">Actualmente, <strong>estamos buscando inversores interesados en participar en emocionantes proyectos en los sectores de importación/exportación, inmobiliario y de restauración</strong>. Nuestra cartera de proyectos incluye oportunidades en mercados internacionales, con un enfoque en la diversificación de riesgos y la generación de retornos atractivos para nuestros inversores</p>
+    <br>
+    <div class="flex text-Texto *:p-10 *:w-full *:*:mb-5">
+        <div>
+            <p class="text-Secundario text-[var(--Principal)]"><strong>Sector de Importación/Exportación</strong></p>
+            <p>Estamos explorando <strong>oportunidades para expandir nuestra presencia en el comercio internacional</strong>. Esto incluye la identificación de nuevos mercados, la optimización de cadenas de suministro y la diversificación de productos para satisfacer las demandas globales.</p>
+        </div>
+        <div>
+            <p class="text-Secundario text-[var(--Principal)]"><strong>Sector Inmobiliario</strong></p>
+            <p>Con el <strong>crecimiento continuo del sector inmobiliario</strong>, estamos desarrollando proyectos innovadores que abarcan desde residencial hasta comercial. Nuestro enfoque se centra en ubicaciones estratégicas, <strong>diseño de calidad y sostenibilidad ambiental.</strong></p>
+        </div>
+        <div>
+            <p class="text-Secundario text-[var(--Principal)]"><strong>Sector de Restauración.</strong></p>
+            <p><strong>Reconocemos el potencial del sector de restauración</strong> en constante evolución. Estamos buscando oportunidades para invertir en conceptos únicos, experiencias gastronómicas innovadoras y tecnologías que mejoren la eficiencia operativa.</p>
+        </div>
+    </div>
+</div>
+
+<div class="py-15 px-8 flex flex-col gap-3 relative text-white mb-15">
+    <div class="absolute inset-0 left-[-33vw] right-[25%] z-[-1] bg-[rgb(16,7,9)] bg-[url($lib/assets/images/pattern.png)]"></div>
+    <h1 class="text-Principal text-[var(--Principal)]">Contacto</h1>
+    <div class="w-1/2 flex flex-col gap-8">
+        <p class="text-Enfasis">Únete a nuestra familia de inversionistas</p>
+        <p class="text-Texto">Si estás buscando oportunidades de inversión sólidas respaldadas por una empresa con experiencia y visión estratégica, te invitamos a unirte a nuestra <strong>familia de inversionistas en Fuster&Lopez</strong>.Contáctanos hoy mismo para obtener más información y explorar cómo podemos colaborar para alcanzar el éxito juntos. En Fuster&Lopez, <strong>estamos comprometidos con la excelencia, la transparencia y la creación de valor a largo plazo</strong>. ¡Esperamos poder trabajar contigo y hacer realidad proyectos exitosos en los mercados globales!
+            <br>Este contenido resalta los aspectos clave de la empresa patrimonial, su enfoque en el <strong>control estratégico y la búsqueda de inversores para proyectos específicos en diferentes sectores</strong>. Puedes adaptarlo según las necesidades y características particulares de tu empresa.</p>
+    </div>
+</div>
